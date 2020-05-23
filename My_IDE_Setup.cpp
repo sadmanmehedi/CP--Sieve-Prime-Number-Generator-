add524b5c9f64fa153dcd2363bf146fcd0b77fd4
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2020-05-23 19:58:50 +0600

    Create My_IDE_Setup.cpp

---
+++ My_IDE_Setup.cpp
@@ -0,0 +1,23 @@
+///HELLO THERE~!
+///CREATED BY MD SADMAN MEHEDI SIVAN(IUT CSE-SWE^19)
+
+#include <bits/stdc++.h>
+
+#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define read freopen("blue.txt","r",stdin);
+#define write freopen("red.txt","w",stdout);
+#define pie 2*acos(0.0);
+
+typedef long long ll;
+
+using namespace std;
+
+int  main()
+{
+
+    fastio;
+
+   
+    return 0;
+
+}

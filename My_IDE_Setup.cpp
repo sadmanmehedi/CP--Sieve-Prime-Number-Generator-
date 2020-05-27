a2cebed5731f614f29ea8d3b9a4764dcc9782b2f
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2020-05-27 12:47:52 +0600

    Update My_IDE_Setup.cpp

--- My_IDE_Setup.cpp
+++ My_IDE_Setup.cpp
@@ -1,23 +1,52 @@
+
 ///HELLO THERE~!
-///CREATED BY MD SADMAN MEHEDI SIVAN(IUT CSE-SWE^19)
-
-#include <bits/stdc++.h>
-
-#define fastio ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define read freopen("blue.txt","r",stdin);
-#define write freopen("red.txt","w",stdout);
-#define pie 2*acos(0.0);
-
-typedef long long ll;
-
+///CREATED BY MD SADMAN MEHEDI SIVAN [IUT CSE(SWE)-19]
+ 
+//#pragma GCC target ("avx2")
+//#pragma GCC optimization ("O3")
+//#pragma GCC optimization ("unroll-loops")
+//#pragma comment(linker, "/stack:200000000")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+ 
+#include<bits/stdc++.h>
 using namespace std;
-
+ 
+#define fastio          ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define read            freopen("blue.txt","r",stdin);
+#define write           freopen("red.txt","w",stdout);
+#define nl              '\n'
+#define sq(a)           ((a) * (a))
+#define vpii            vector<pair<int,int>>
+#define vpll            vector<pair<long long,long long>>
+#define pb              push_back
+#define mp              make_pair
+#define ff              first
+#define ss              second
+#define SORT(v)         sort(v.begin(),v.end())
+#define rsort(v)        sort(v.rbegin(), v.rend())
+#define rev(v)          reverse(v.begin(),v.end())
+#define mem(a)          memset(a,-1,sizeof a)
+#define clr(a)          memset(a,0,sizeof a)
+#define GCD(a,b)        __gcd(a,b)
+#define LCM(a,b)        (a / __gcd(a,b) ) *b
+#define precision(a)    fixed << setprecision(a)
+#define min3(a,b,c)     min(a,min(b,c))
+#define max3(a,b,c)     max(a,max(b,c))
+#define min4(a,b,c,d)   min(a,min(b,min(c,d)))
+#define max4(a,b,c,d)   max(a,max(b,max(c,d)))
+#define DEBUG(x) cerr << #x << " = " << x << endl
+ 
+using ll=long long;
+using ull=unsigned long long;
+ 
+const double pi = 2*acos(0.0);
+ 
 int  main()
 {
-
+ 
     fastio;
-
-   
+ 
+ 
     return 0;
-
 }

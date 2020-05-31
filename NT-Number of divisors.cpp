ddb706e129cff0455f64a26b8f503f25c0ef716d
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2020-05-31 12:08:33 +0600

    Update and rename NT-FIND ALL DIVISORS OF A NUMBER.cpp to NT-Number of divisors.cpp

--- NT-Number of divisors.cpp
+++ NT-Number of divisors.cpp
@@ -15,37 +15,18 @@ void sieve()
                 flag[i*j] = false;
 
 
-
     primes.push_back(2);
     for(long long i = 3; i < 1000000; i += 2)
         if(flag[i])
             primes.push_back(i);
 
 }
-///NOT EFFICIENT ALGO
-  
-void printDivisors(int n) 
-{ 
-  
-    for (int i=1; i<=sqrt(n); i++) 
-    { 
-        if (n%i == 0) 
-        { 
-            
-            if (n/i == i) 
-                printf("%d ", i); 
-  
-            else 
-                printf("%d %d ", i, n/i); 
-        } 
-    } 
-} 
 
-///EFFICIENT ALGO
 long long countDivisor(long long n)
 {
     long long divisor = 1;
-    for (long long i = 0; i < primes.size(); i++)
+    long long p=primes.size();
+    for (long long i = 0; primes[i]*primes[i] <=n && i<p ; i++)
     {
         if (n % primes[i] == 0)
         {
@@ -58,12 +39,15 @@ long long countDivisor(long long n)
             divisor *= cnt;
         }
     }
+    if(n>1)
+        divisor*=2;
     return divisor;
 }
+
 int main()
 {
-  long long n;
-  cin>>n;
-  countDivisior(n);
-  return 0;
+    sieve();
+    countDivisor(n);
+ 
+    return 0;
 }

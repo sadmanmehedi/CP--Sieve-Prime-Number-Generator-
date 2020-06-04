303c37c5508b8587889dcbed38ad2c1a13107a75
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2020-06-04 16:19:09 +0600

    Create NT-Eular Totient Function.cpp

---
+++ NT-Eular Totient Function.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h> 
+  
+int phi(int n) 
+{ 
+    int result = n; // Initialize result as n 
+  
+    // Consider all prime factors of n and subtract their 
+    // multiples from result 
+    for (int p = 2; p * p <= n; ++p) { 
+          
+        // Check if p is a prime factor. 
+        if (n % p == 0) { 
+              
+            // If yes, then update n and result 
+            while (n % p == 0) 
+                n /= p; 
+            result -= result / p; 
+        } 
+    } 
+  
+    // If n has a prime factor greater than sqrt(n) 
+    // (There can be at-most one such prime factor) 
+    if (n > 1) 
+        result -= result / n; 
+    return result; 
+} 
+  
+int main() 
+{ 
+    int n; 
+    for (n = 1; n <= 10; n++) 
+        printf("phi(%d) = %d\n", n, phi(n)); 
+    return 0; 
+    
+    
+    
+  FACTS:
+  1.IF THE NUMBER IS PRIME THEN ANS P-1
+  2.P^K=P^(K-1)*(P-1)

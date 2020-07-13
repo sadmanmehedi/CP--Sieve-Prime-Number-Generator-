7a342de997ea2175d707bfa7baf0e368dac9de92
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2020-07-13 16:45:56 +0600

    Update NT-sieve.cpp

--- NT-sieve.cpp
+++ NT-sieve.cpp
@@ -1,25 +1,43 @@
-#include <bits/stdc++.h>
+
+#include<bits/stdc++.h>
 using namespace std;
 
 vector<bool> flag(1000000, true);
-vector<int> primes;
+vector<long long> primes;
 
-void sieve() {
-	flag[0] = false;
-	flag[1] = false;
+void sieve()
+{
+    flag[0] = false;
+    flag[1] = false;
 
-	for(int i = 2; i*i < 1000000; i++)
-		if(flag[i])
-			for(int j = 2; i*j < 1000000; j++)
-				flag[i*j] = false;
+    for(long long i = 2; i*i < 1000000; i++)
+        {
+            if(flag[i])
+            {
+                for(long long j = 2; i*j < 1000000; j++)
+                {
+                    flag[i*j] = false;
+                }
+            }
+        }
 
-	/*
-	primes.push_back(2);
-	for(int i = 3; i < 1000000; i += 2)
-		if(flag[i])
-			primes.push_back(i);
-	*/
+
+    primes.push_back(2);
+    for(long long i = 3; i < 1000000; i += 2)
+        {
+            if(flag[i])
+            primes.push_back(i);
+        }
+
 }
 
-int main() { }
-///BITWISE SIEVE SHIKTE HOBE
+int main()
+{
+    sieve();
+
+
+
+
+    return 0;
+}
+

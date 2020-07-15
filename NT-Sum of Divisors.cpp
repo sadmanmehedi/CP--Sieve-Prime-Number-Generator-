e8d4a68aaa0d2f804b3a5e4d97f5b795560be241
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2020-07-15 16:18:36 +0600

    Update NT-Sum of Divisors.cpp

--- NT-Sum of Divisors.cpp
+++ NT-Sum of Divisors.cpp
@@ -1,83 +1,96 @@
 ///PROBLEM LINK:https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3194
 ///VIDEO REF:https://www.youtube.com/watch?v=x9KMLQa6G5w
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define MAX 32000
+
 using namespace std;
 
-vector<bool> flag(10000000, true);
+using ll = long long;
+
 vector<long long> primes;
 
 void sieve()
 {
-    flag[0] = false;
-    flag[1] = false;
+    bool isPrime[MAX];
 
-    for(long long i = 3; i*i < 10000000; i+=2)
+    for (long long i = 0; i < MAX; ++i)
+        isPrime[i] = true;
+
+    for (long long i = 3; i * i <= MAX; i += 2)
+    {
+        if (isPrime[i])
         {
-            if(flag[i])
+            for (long long j = i * i; j <= MAX; j += i)
             {
-                for(long long j = i; i*j < 10000000; j++)
-                {
-                    flag[i*j] = false;
-                }
+                isPrime[j] = false;
             }
         }
-
+    }
 
     primes.push_back(2);
-    for(long long i = 3; i < 10000000; i += 2)
-        {
-            if(flag[i])
-            {
-                primes.push_back(i);
-            }
-        }
 
-}
-
-
-
-
-///NOT EFFICIENT ALGO
-
-int divisorSum1(int n) {
-    int cnt = 0;
-    for (int i = 1; i * i <= n; i++) {
-        if (i * i == n) {
-            cnt += i;
-        } else if (n % i == 0) {
-            cnt += (i + n/i);
-        }
-    }
-    return cnt;
-}
-
-///EFFICIENT ALGO
-
-long long  divisorSum2(long long  n)
-{
-    if (n < 1) return 0;
-    long long sum = 1;
-    long long p=primes.size();
-    for (long long  i = 0; primes[i]*primes[i]<= n && i<p; i++)
+    for (long long i = 3; i < MAX; i += 2)
     {
-        if (n % primes[i] == 0)
-        {
-            long long  cnt = 1;
-            while (n % primes[i] == 0)
-            {
-                n /= primes[i];
-                cnt++;
-            }
-            sum *= (powl(primes[i], cnt) - 1) / (primes[i] - 1);
-        }
+        if (isPrime[i]) primes.push_back(i);
     }
-    if (n > 1) sum *= (powl(n, 2) - 1) / (n - 1);
-    return sum;
+}
+
+void segSieve (ll l, ll r)
+{
+    bool isPrime[r-l+1];
+   
+    for (long long i = 0; i < r - l + 1; ++i)
+        isPrime[i] = true;
+   
+    if (l == 1) isPrime[0] = false;
+   
+    for (long long i = 0; primes[i]*primes[i] <= r; ++i)
+    {
+        long long currentPrime = primes[i];
+        
+        ll base = (l/currentPrime)*currentPrime;
+        
+        if (base < l) 
+        base += currentPrime;
+        
+        for (ll j = base; j <= r; j += currentPrime)
+        {
+            isPrime[j-l] = false;
+        }
+        
+        if (base == currentPrime) 
+            isPrime[base-l] = true;
+    
+    }
+    
+    for (long long i = 0; i < r - l + 1; ++i)
+    {
+        if (isPrime[i]) 
+            cout << (i+l) << endl;
+    }
+    
+    puts("");
+    
 }
 
 int main()
 {
     sieve();
     
+    long long t;
+    cin >> t;
+   
+    while (t--)
+    {
+        
+        ll l, r;
+        cin >> l >> r;
+        segSieve(l, r);
+    }
+
+
+    return 0;
+
 }
+

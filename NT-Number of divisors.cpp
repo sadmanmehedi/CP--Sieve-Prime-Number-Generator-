d682291ade368aa6302fb12ce77403015d0cf7ff
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2020-07-15 13:58:37 +0600

    Update NT-Number of divisors.cpp

--- NT-Number of divisors.cpp
+++ NT-Number of divisors.cpp
@@ -5,26 +5,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<bool> flag(1000000, true);
-vector<long long > primes;
+vector<bool> flag(10000000, true);
+vector<long long> primes;
 
 void sieve()
 {
     flag[0] = false;
     flag[1] = false;
 
-    for(long long  i = 2; i*i < 1000000; i++)
-        if(flag[i])
-            for(long long  j = 2; i*j < 1000000; j++)
-                flag[i*j] = false;
+    for(long long i = 3; i*i < 10000000; i+=2)
+        {
+            if(flag[i])
+            {
+                for(long long j = i; i*j < 10000000; j++)
+                {
+                    flag[i*j] = false;
+                }
+            }
+        }
 
 
     primes.push_back(2);
-    for(long long i = 3; i < 1000000; i += 2)
-        if(flag[i])
-            primes.push_back(i);
+    for(long long i = 3; i < 10000000; i += 2)
+        {
+            if(flag[i])
+            {
+                primes.push_back(i);
+            }
+        }
 
 }
+
+
+
 
 long long countDivisor(long long n)
 {
@@ -47,6 +60,8 @@ long long countDivisor(long long n)
         divisor*=2;
     return divisor;
 }
+
+
 
 int main()
 {

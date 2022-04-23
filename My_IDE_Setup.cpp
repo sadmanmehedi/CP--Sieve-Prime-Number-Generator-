e5d76e19823a234d431e1f84e20dbf2cb873b380
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2022-04-24 05:17:08 +0600

    Update My_IDE_Setup.cpp

--- My_IDE_Setup.cpp
+++ My_IDE_Setup.cpp
@@ -15,16 +15,6 @@ const double pi = 2*acos(0.0);
 const long mx = LONG_MAX;
 const long mn = LONG_MIN;
 
-long long vis1d[3000001];
-long long dis1d[3000001];
-
-vector<ll>graph[3000001];
-
-long long vis[1001][1001];
-long long dis[1001][1001];
-
-long long disx[]= {-2,-2,-1,-1,2,2,1,1};
-long long disy[]= {1,-1,2,-2,1,-1,-2,2};
 
 int main()
 {

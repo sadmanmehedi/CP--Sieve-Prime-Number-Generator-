8a2eed4899e6a7fadf4d398ff602f1aa604370ea
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2022-05-14 10:30:19 +0600

    DiviorsMonekyWay
    NOT A GOOOD WAY

---
+++ Number Theory/DivisorsHumanWay.cpp
@@ -0,0 +1,31 @@
+///O(N)
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FASTIO          ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define READ            freopen("blue.txt","r",stdin);
+#define WRITE           freopen("red.txt","w",stdout);
+
+using ll=long long;
+using ull=unsigned long long;
+
+const double pi = 2*acos(0.0);
+const long mx = LONG_MAX;
+const long mn = LONG_MIN;
+
+int main()
+{
+
+        long long n;
+        cin>>n;
+
+        for(long long i=1;i<=n;i++)
+        {
+            if(n%i==0)
+                cout<<i<<" ";
+        }
+
+        return 0;
+
+    }

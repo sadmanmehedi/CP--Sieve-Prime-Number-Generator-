ad760e95c1737de0ea9d792f7e45b1794cc63048
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2022-06-18 13:19:45 +0600

    Updated with querysum now

--- SegmentTree/1SegmentTreeBasic.cpp
+++ SegmentTree/1SegmentTreeBasic.cpp
@@ -20,7 +20,7 @@ const long long N=1e5 + 9 ;
 long long a[N];
 long long t[N*4];//Node wise sum er value rakhbo
 
-void build(long long node,long long b,long long e)
+void build(long long node,long long b,long long e)///Tree Build Kortesi
 {
 
 if(b==e)
@@ -41,12 +41,32 @@ build(r,mid+1,e);
 
 t[node]=t[l]+t[r];
 
+}
+
+
+long long querysum(long long node,long long b,long long e,long long i,long long j)///eita amar range sum dibe as kichu node a hoito amar iccha moto range nai and amra i & j use kortesi as l & r vitore use kora porse
+{
+    if(b>j or e<i)
+        return 0;
+
+    if(b>=i and e<=j)
+        return t[node];//karon niche jaoar dorkar nei
+
+    long long l=2*node;
+    long long r=2*node+1;
+    
+    long long mid=(b+e)/2;
+
+    return querysum(l,b,mid,i,j)+querysum(r,mid+1,e,i,j);
+
+
 
 
 }
+
+
 int main()
 {
-
     long long n=1000;
 
     for(long long i=1;i<=n;i++)
@@ -57,7 +77,9 @@ int main()
 
     build(1,1,n);
 
-    cout<<t[1];
+    cout<<t[1]<<endl;
+
+    cout<<querysum(1,1,n,2,6);
 
         return 0;
     }

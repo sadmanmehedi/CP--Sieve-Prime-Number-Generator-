eb0eb91512d8720e144e10a2fe7c082fd590ebe6
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2022-06-29 13:59:03 +0600

    Update 8.Discovery&FinishingtimeDFS.cpp

--- GRAPHS/8.Discovery&FinishingtimeDFS.cpp
+++ GRAPHS/8.Discovery&FinishingtimeDFS.cpp
@@ -1,1 +1,21 @@
-https://www.youtube.com/watch?v=HC5EpkPpwAE&list=PLgLCjVh3O6Sim5bsg0FJ3qGbvM6qtdxQe&t=0s
+///https://www.youtube.com/watch?v=4VCqVBnUrDY&list=PL5DyztRVgtRVLwNWS7Rpp4qzVVHJalt22&index=12
+
+
+long long timer=1;
+
+void dfs(long long v)
+{
+    vis[v]=1;
+    IN[v]=timer++;
+    
+    for(auto u:v)
+    {
+        if(vis[u]==0)
+        {
+            dfs(u);
+        }
+    }
+    
+    
+    OUT[v]=timer++;
+}

85ef7c2f4c33c23942ef9b2488642bf8e01436ef
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2022-10-27 16:59:08 +0600

    modified vector

--- STL/3.vector.cpp
+++ STL/3.vector.cpp
@@ -13,9 +13,9 @@ int main()
 
     ///iterator works as a memory address,it points
 
-    vector <int>::iterator it=v.end();///last er porer index a thakbe
-    vector <int>::iterator bit=v.rend();///last er porer index a thakbe
-    vector <int>::iterator ist=v.rbegin();///last er porer index a thakbe
+    vector <int>::iterator it=v.end(); //last er porer index a thakbe
+    vector <int>::reverse_iterator bit=v.rend();///vector k reverse kore then last index er pore
+    vector <int>::reverse_iterator ist=v.rbegin();///reverse kore first index,++ korle bame jabe
 
 
     cout<<v[0]<<" "<<v.at(0)<<endl;
@@ -23,6 +23,29 @@ int main()
 
     for(vector<int>::iterator it=v.begin(); it!=v.end(); it++)
         cout<<*(it);
+
+
+    ///auto=iterator er sob kaj korte pare.
+
+
+    for(auto rit:v)
+        cout<<rit;///no * neeeded
+
+
+
+    ///DELETION PART
+
+        v.erase(v.begin()+1);///index 1 will be deleted
+
+
+        v.erase(v.begin()+1,v.begin()+3);///index 1 and 2 will be deleted not 3,it works like this [)
+
+        ///INSERTATION PART
+
+        v.insert(v.begin+2(1000));///2 index a 1000 insert hoye jabe
+        v.insert(v.begin(),2,10);///0 index a 2 ta 10 push hoye jabe
+
+
 
     return 0;
 

28994d531527eb1a3a2ca2fca1afe8221936952a
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2022-10-27 16:43:38 +0600

    working with vector

--- STL/3.vector.cpp
+++ STL/3.vector.cpp
@@ -11,8 +11,18 @@ int main()
 
     vector<int>vec(5);///(0,0,0,0,0)
 
+    ///iterator works as a memory address,it points
+
+    vector <int>::iterator it=v.end();///last er porer index a thakbe
+    vector <int>::iterator bit=v.rend();///last er porer index a thakbe
+    vector <int>::iterator ist=v.rbegin();///last er porer index a thakbe
 
 
+    cout<<v[0]<<" "<<v.at(0)<<endl;
+
+
+    for(vector<int>::iterator it=v.begin(); it!=v.end(); it++)
+        cout<<*(it);
 
     return 0;
 

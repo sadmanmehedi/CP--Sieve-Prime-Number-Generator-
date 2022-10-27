176d08ec810fe7cef23bc9da6dd3412c6589212b
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2022-10-27 16:37:42 +0600

    vector will be added

---
+++ STL/3.vector.cpp
@@ -0,0 +1,19 @@
+///CODED BY SADMAN MEHEDI SIVAN
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    vector<int>v(5,1000);///(1000,1000,1000,1000,1000)
+
+    vector<int>vec(5);///(0,0,0,0,0)
+
+
+
+
+    return 0;
+
+}

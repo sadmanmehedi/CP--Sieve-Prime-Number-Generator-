fb26c09dc81d85dff82086e7cf70d6fe8d50f242
SADMAN MEHEDI SIVAN <56332913+sadmanmehedi@users.noreply.github.com>
2022-11-08 19:29:42 +0600

    Update Binarysearch.cpp

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -1,61 +1,69 @@
-///RECOMMENDED VIDEO:CODING BLOCKS,ERRICHTO
-///PRACTICE CODECHEF,HACCKERRANK,CODEFORCES,CSES AND TOPIC WISE PROBLEM SHEET
+/*Monotonic Function->Jeita ekta proper sequence maintain kore
+like
+sorted
+sorted in decreasing order
+FFFFTTTTT
+TTTTFFFFF
 
+Predicate Function:Ekta dheu er moton graph(Eigulate Binary Search use kora jabena)
+
+*/
+
+
+///CODED BY SADMAN MEHEDI SIVAN
 
 #include<bits/stdc++.h>
 using namespace std;
 
-long long binarysearch(long long arr[],long long n,long long key)
+long long binarysearch(long long arr[],long long n,long long x)
 {
+
     long long low=0,high=n-1;
-    long long mid;
 
     while(low<=high)
     {
-        mid=low+(high-low)/2;
-        if(arr[mid]==key)
-        {
-            return mid;
-        }
-        else if(arr[mid]<key)
-        {
-            low=mid+1;
-        }
-        else if(arr[mid]>key)
-        {
-            high=mid-1;
-        }
+
+    long long mid=(low+high)/2;
+
+    if(arr[mid]==x)
+    return mid;
+
+    if(x>arr[mid])
+    {
+        low=mid+1;
+    }
+    else
+    {
+        high=mid-1;
     }
 
-    return -1;
+    }
+
+    return  -1 ;
 
 }
-
-
 
 int main()
 {
 
-    long long arr[]= {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-    long long n=sizeof(arr)/sizeof(long long);
+   long long n;
+   cin>>n;
 
-    long long key;
-    cin>>key;
+   long long a[n+1];
 
-    long long searchindex=binarysearch(arr,n,key);
+   for(long long i=0;i<n;i++)
+   cin>>a[i];
 
-    if(searchindex==-1)
-    {
-        cout<<"NOT FOUND!";
-    }
+   long long x;
+   cin>>x;
 
-    else
-    {
-        cout<<"THE ELEMENT "<<key<<" IS FOUND IN "<<searchindex<<" INDEX OF THE ARRAY.";
-    }
+   cout<<binarysearch(a,n,x)<<endl
+   ;
+
+
+
 
     return 0;
-
 
 }
 

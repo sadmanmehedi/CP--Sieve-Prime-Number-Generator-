86c750999edb3f86ca28019e030a75beab6e0422
SADMAN MEHEDI SIVAN <sadman.mehedi@gmail.com>
2022-11-28 04:43:11 +0600

    Created Two Sum

---
+++ Two_Pointers/Threesum.cpp
@@ -0,0 +1,58 @@
+
+//Brute Force N^3 & Using Two Pointers makes it N^2
+
+///CODED BY SADMAN MEHEDI SIVAN
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long n,x;
+    cin>>n>>x;
+
+    long long a[n+1];
+
+    vector<pair<long long,long long>>v;
+    for(long long i=0; i<n; i++)
+    {
+        cin>>a[i];
+        v.push_back({a[i],i+1});
+    }
+
+    sort(v.begin(),v.end());
+
+
+
+    for(long long i=0; i<n; i++)
+    {
+        long long l=i+1;
+        long long r=n-1;
+        while(l<r)
+        {
+            long long sum=v[i].first+v[l].first+v[r].first;
+
+            if(sum==x)
+            {
+                cout<<v[i].second<<" "<<v[l].second<<" "<<v[r].second<<endl;
+                return 0;
+
+            }
+            if(sum>x)
+            {
+                r--;
+            }
+            if(sum<x)
+                l++;
+        }
+    }
+
+    cout<<"IMPOSSIBLE";
+
+
+    return 0;
+}
+
+
+
